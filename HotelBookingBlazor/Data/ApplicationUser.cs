c8f6cd22f6902679e313be8e6f266edac27b060c
VascoSilva-Dev <sprvasco@hotmail.com>
2024-01-09 16:59:03 +0000

    Initial commit

---
+++ HotelBookingBlazor/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelBookingBlazor.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
